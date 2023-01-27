4401a89ef448eb5f05844c369f4f41d388e5a745
emontes22 <ervey025@gmail.com>
2023-01-26 21:36:59 -0700

    Complete server side functionality

--- server.py
+++ server.py
@@ -4,6 +4,42 @@ import threading
 HOST = '127.0.0.1'
 PORT = 8080
 USER_LIMIT = 5
+active_clients = [] # Connected users
+
+# Listen for upcoming messages from a client
+def listen_for_messages(client, username):
+	# Keep listening for messages from the client
+	while True:
+		message = client.recv(2048).decode('utf-8')
+		if message != '':
+			final_msg = username + ':' + message
+			send_messages(final_msg)
+		else:
+			print(f"Message send from client: {username} is empty")
+
+# Send a message to one client
+def send_message_to_client(client, message):
+	client.sendall(message.encode())
+
+# Send message to all clients currently connected to the server
+def send_messages(message):
+	
+	for user in active_clients:
+		send_message_to_client(user[1], message)
+
+
+# Client handling
+def client_handler(client):
+	# Listen to client messages that contain the username
+	while True:
+		username = client.recv(2048).decode('utf-8')
+		if username != '':
+			active_clients.append((username, client))
+			break
+		else:
+			print("Username is empty")
+
+	threading.Thread(target=listen_for_messages, args=(client, username, )).start()
 
 def main():
 	# Socket class object
@@ -24,6 +60,8 @@ def main():
 	while True:
 		client, address = server.accept()
 		print(f"Sucessfully connected to client: {address[0]} {address[1]}")
+
+		threading.Thread(target=client_handler, args=(client, )).start()
 
 if __name__ == '__main__':
 	main()
